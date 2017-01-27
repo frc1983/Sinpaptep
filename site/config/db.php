57183907d91846889ba9fb9984571c05a0b27574
Fábio <fabiorochadacosta@gmail.com>
2017-01-27 12:40:12 -0200

    Ajuste de localhost

--- site/config/db.php
+++ site/config/db.php
@@ -2,8 +2,8 @@
 
 return [
     'class' => 'yii\db\Connection',
-    'dsn' => 'mysql:host=localhost;dbname=sindicat_site',
-    'username' => 'sindicat_site',
-    'password' => 'Desliga*11',
+    'dsn' => 'mysql:host=localhost:3307;dbname=sindicat_site',
+    'username' => 'root',
+    'password' => 'usbw',
     'charset' => 'utf8',
 ];
