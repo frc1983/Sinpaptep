2127ec05be35c693dce2f19ddd932178891d2963
Fábio <fabiorochadacosta@gmail.com>
2018-03-18 21:14:11 -0300

    Finalizando

--- admin/models/User.php
+++ admin/models/User.php
@@ -13,18 +13,11 @@ class User extends \yii\base\Object implements \yii\web\IdentityInterface
     private static $users = [
         '100' => [
             'id' => '100',
-            'username' => 'admin',
-            'password' => 'admin',
+            'username' => 'manoel',
+            'password' => 'maneca*78',
             'authKey' => 'test100key',
             'accessToken' => '100-token',
-        ],
-        '101' => [
-            'id' => '101',
-            'username' => 'demo',
-            'password' => 'demo',
-            'authKey' => 'test101key',
-            'accessToken' => '101-token',
-        ],
+        ]
     ];
 
     /**
