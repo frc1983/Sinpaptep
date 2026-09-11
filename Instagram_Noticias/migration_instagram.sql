ff0d4b01402d4164e2b8f2a9d7183889c5207cb9
Fábio Rocha da Costa <fabiorochadacosta@gmail.com>
2026-09-11 08:23:38 -0300

    Remove modulo legado Instagram Noticias

--- Instagram_Noticias/migration_instagram.sql
+++
@@ -1,7 +0,0 @@
--- Migração: Adicionar suporte a posts do Instagram nas notícias
--- Execute este script no banco de dados sinpaptep
-
-ALTER TABLE `Noticia`
-    ADD COLUMN `Instagram_Url` VARCHAR(500) NULL DEFAULT NULL
-        COMMENT 'URL do post do Instagram para embed (ex: https://www.instagram.com/p/CODE/)'
-    AFTER `Texto`;
